31924d3f793f6ac8e5cab8d954cd8ed7a881df14
shashank rao <shashankraos.xworkz@gmail.com>
2022-04-06 12:22:13 +0530

    cards

--- learnlytica/src/app/components/course/course.component.ts
+++ learnlytica/src/app/components/course/course.component.ts
@@ -12,12 +12,12 @@ export class CourseComponent implements OnInit {
 //   price:2000,
 //   name:'shashank'
 // }
-card1=[{
+card=[{
   title:'Git For Beginners',
   price:'2000',
   name:'shashank',
   currentdt:new Date(),
-  imagelink:'https://dz8fbjd9gwp2s.cloudfront.net/courses/61aa18860cf26833c8eca46c/61aa18860cf26833c8eca46c_scaled_cover.jpg?v=1'
+  imagelink:'https://dz8fbjd9gwp2s.cloudfront.net/courses/61a9e34f0cf2f90364772b94/61a9e34f0cf2f90364772b94_scaled_cover.jpg?v=1'
 },
 {
   title:'Mean For Beginners',
@@ -31,7 +31,7 @@ card1=[{
   price:'10000',
   name:'nandan',
   currentdt:new Date(),
-  imagelink:'https://dz8fbjd9gwp2s.cloudfront.net/courses/61aa18860cf26833c8eca46c/61aa18860cf26833c8eca46c_scaled_cover.jpg?v=1'
+  imagelink:'https://dz8fbjd9gwp2s.cloudfront.net/courses/61ae538d0cf272041d79ddd9/61ae538d0cf272041d79ddd9_scaled_cover.jpg?v=1'
 }
 ];
   constructor() { }
