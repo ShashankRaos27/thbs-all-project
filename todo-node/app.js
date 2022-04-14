1da9e22b097123f5f6ac368bc27176d6a0fae062
shashank rao <shashankraos.xworkz@gmail.com>
2022-04-14 15:00:58 +0530

    sample node prg

--- todo-node/app.js
+++ todo-node/app.js
@@ -1,13 +1,18 @@
-const http=require('http');
-const hostname='127.0.0.1';
-const port=3000;
-const webserver=http.createServer(
-    (req,res)=>{
-        res.statusCode=200;
-        res.setHeader('content-type','text/plain');
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+require('dotenv').config();
+console.log(process.env.user_id);
+console.log(process.env.user_key);
+const webserver = http.createServer(
+    (req, res) => {
+        console.log(req.headers);
+
+        res.statusCode = 200;
+        res.setHeader('content-type', 'text/plain');
         res.end('hello world my first response');
     }
 )
-webserver.listen(port,hostname,()=>{
+webserver.listen(port, hostname, () => {
     console.log(`server is running at http://${hostname}:${port}/`);
 })
