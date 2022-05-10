a183c10bf9a54258ea5baf706de1b17acbf23cdd
shashank rao <shashankraos.xworkz@gmail.com>
2022-05-10 13:25:27 +0530

    added

--- api-learnlytica/db/db.js
+++ api-learnlytica/db/db.js
@@ -20,13 +20,20 @@
 // connection.end();
 
 var mysql = require('mysql2');
+require('dotenv').config();
 let connection = function () {
     let mySql= mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: '2708',
-        database: 'api',
-        insecureAuth : true
+        // host: 'localhost',
+        // user: 'root',
+        // password: '2708',
+        // database: 'api',
+        // insecureAuth : true
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD, //should be yours 
+        database: process.env.DB_NAME,
+        port: process.env.DB_PORT,
+        insecureAuth: true
     });
     return mySql;
 };
